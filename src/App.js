c461cbb9827d7a862e7c37fce82850b283ac385a
Om Thanage <om.thanage@somaiya.edu>
2024-03-27 00:39:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ShiningStars from './Components/ShiningStars';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ShiningStars />
+    </div>
+  );
+}
+
+export default App;
