0d1aa690d5a4419e3583e914e3ca18dddb0f506f
Piyush Chaudhary <piyushchy05@gmail.com>
2024-03-28 17:46:22 +0530

    Adjusted CSS to ensure consistent spacing throughout the website

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import earth from './earth.png'
 import logo from './logo.png'
-
+import './Home.css'
 function Home() {
   return (
-    <div className='text-center text-white m-3'>
-      <div className='row justify-content-center'>
+    <div className='text-center text-white m-4'>
+      <div className='row justify-content-center ssrp-absolute'>
         <h1 className='d-inline-block'>SSRP</h1>
       </div>
-      <div className='row justify-content-center'>
+      <div className='row justify-content-center ssrp-absolute' style={{marginTop:'4.5rem'}}>
         <h3 className='d-inline-block'>Somaiya Space Research Project</h3>
       </div>
       <div className='row justify-content-end m-3'> 
-        <div className='col-6'>
+        <div className='col-6 mt-3'>
           <img className='image img-fluid' src={earth} alt='earth'/>
-          <img className='logo img-fluid positioin-absolute' style={{transform: 'translate(-50% , -50%)'}} src={logo} alt='logo of team ssrp' />
+          <img className='logo img-fluid positioin-absolute' style={{marginTop:'-50%', marginLeft:'-50%'}} src={logo} alt='logo of team ssrp' />
         </div>
       </div>
-      <div style={{ marginTop: '-20vh'}}>
+      <div className='m-4'>
       <p className='Slogan'>From Dreams to Reality</p>
       <section id = 'about'>
         <h2>About Us</h2>
