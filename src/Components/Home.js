91c82cdeb757771b2eda4df87710bf951f68b1ce
Om Thanage <om.thanage@somaiya.edu>
2024-03-28 14:25:10 +0530

    added responsive design to the navbar and home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,12 +8,14 @@ function Home() {
       <section>
         <h1 className='title'>SSRP</h1>
         <h3 className='title'>Somaiya Space Research Project</h3>
-        
-
       </section>
         <img className='image' src={earth} alt='earth' />
         <img className='logo' src={logo} alt='logo of team ssrp' />
         <p className='Slogan'>From Dreams to Reality</p>
+      <section id = 'about'>
+        <h2>About Us</h2>
+        <p>SSRP is a student-led initiative at K.J. Somaiya College of Engineering, Mumbai. We aim to foster a culture of space research and innovation among students. Our goal is to provide a platform for students to work on real-world space projects and gain hands-on experience in the field of space technology.</p>
+      </section>
     </div>
   )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,41 +1,42 @@
 
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
-function NavBar() {
+function App() {
   return (
-    <div className="NavBar">
+    <div className="App" style={{ width: "100vw" }}>
    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
 
-   
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-        <div className="navbar-brand">
-        <img src="https://i.imgur.com/miZ2Zyz.png" alt="Logo" style={{maxHeight: "50px", width: "auto", objectFit: "contain", marginRight: "10px"}} className='mr-2'/>
-  <span className="ml-2">Home</span>
-</div>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-  <ul className="navbar-nav">
-    <li className="nav-item mr-2">
-      <a className="nav-link" href="#">Projects</a>
-    </li>
-    <li className="nav-item mr-2">
-      <a className="nav-link" href="#">Community</a>
-    </li>
-    <li className="nav-item mr-2">
-      <a className="nav-link" href="#">Connections</a>
-    </li>
-    <li className="nav-item mr-2">
-      <a className="nav-link" href="#">Contact Us</a>
-    </li>
-  </ul>
-</div>
-        
-        </div>
-      </nav>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixnav">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src='https://i.imgur.com/miZ2Zyz.png' style={{maxHeight: "50px", width: "auto", objectFit: "contain", marginRight: "10px"}}></img>
+      Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse bg-dark" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item mr-lg-5">
+          <a class="nav-link active" aria-current="page" href="#">Projects</a>
+        </li>
+        <li class="nav-item mr-lg-5">
+          <a class="nav-link active" aria-current="page" href="#">Community</a>
+        </li>
+        <li class="nav-item mr-lg-5">
+          <a class="nav-link active" aria-current="page" href="#">Connections</a>
+        </li>
+        <li class="nav-item mr-lg-5">
+          <a class="nav-link active mr-lg-5" href="#">Contact US</a>
+        </li>
+      
+       
+        </ul>
+     
+    </div>
+  </div>
+</nav>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
    
@@ -45,4 +46,4 @@ function NavBar() {
   );
 }
 
-export default NavBar;
+export default App;
