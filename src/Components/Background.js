4a2d8a847bf325f512ec0217e84ef4c81cf7af26
Om Thanage <om.thanage@somaiya.edu>
2024-03-29 17:19:09 +0530

    fixed background image issue

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -10,7 +10,7 @@ const StarBackground = ({ children }) => {
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
         const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.offsetwidth, window.innerHeight);
+        renderer.setSize(window.innerWidth, window.innerHeight);
         containerRef.current.appendChild(renderer.domElement);
         rendererRef.current = renderer;
 
