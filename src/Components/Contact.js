863e40626dc0d002bd81abd7ea3b1752e934bbf7
Om Thanage <om.thanage@somaiya.edu>
2024-03-30 15:02:43 +0530

    ....

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -13,10 +13,10 @@ function Contact() {
             <input type="text" name="name" placeholder="Your Name" class="contact-inputs" required></input>
             <input type="email" name="email" placeholder="Your Email" class="contact-inputs" required></input>
             <textarea name="message" placeholder="Your Message" class="contact-inputs" required></textarea>
-            <button type="submit">Submit <img src={arrow} alt="" /></button>
+            <button type="submit">Submit <img src={arrow} alt="arrow to submit" /></button>
         </form>
         <div className='contact-right'>
-            <img src={Astronaut} alt="" />
+            <img src={Astronaut} alt="Astronaut" />
         </div>
     </div>
   )
