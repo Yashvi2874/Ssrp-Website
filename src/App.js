104ada7ce4b1937e24312341d38fc85a8365ec5e
Piyush Chaudhary <piyushchy05@gmail.com>
2024-03-30 14:06:51 +0530

    Add Dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 
 import './App.css';
+import React from 'react';
 import Home from './Components/Home';
 import NavBar from './Components/Navbar';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
+import Project from './Components/Project';
 import StarBackground from './Components/Background';
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<React.Fragment><div className="App">
       <div className="stars">
         <StarBackground />
       </div>
@@ -22,7 +27,11 @@ function App() {
           <Footer />
         </div>
       </div>
-    </div>
+    </div></React.Fragment>} />
+    <Route path="projects" element={<Project />} />
+    <Route path="project" element={<Project />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
