898848480cb7590b7916feca821a4809dc61dd17
Om Thanage <om.thanage@somaiya.edu>
2024-03-30 13:48:21 +0530

    redesigned contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
     <div className='contact-container'>
         <form action="" class="contact-left">
             <div class="contact-left-title">
-                <h2>Get in touch</h2>
+                <h2 className='gradient-text'>Ignite the steller exchange 🔥 </h2>
                 <hr />
             </div>
             <input type="text" name="name" placeholder="Your Name" class="contact-inputs" required></input>
