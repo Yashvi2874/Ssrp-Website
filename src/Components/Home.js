f19cc9d2c45a2a39123036c69201bc2f39a6ff4d
Om Thanage <om.thanage@somaiya.edu>
2024-04-02 01:26:47 +0530

    Added Nulshock font and license

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,16 +6,16 @@ import TypeIt from 'typeit-react';
 
 function Home() {
   return (
-    
+    <div className='Home'>
     <div className='text-center text-white m-4'>
       <div className='row justify-content-center ssrp-absolute'>
       <TypeIt className='typeit-text' options={{ strings: "SSRP", speed: 50, waitUntilVisible: true, cursor: false }} />
 <TypeIt className='typeit-text' id='small-type' options={{ strings: "Somaiya Space Research Project", speed: 30, waitUntilVisible: true, cursor: false }} />
       <p className='Slogan'>Launching dreams into orbits</p>
       </div>
-      <div className='row justify-content-center ssrp-absolute' style={{ marginTop: '4.5rem' }}>
+      {/* <div className='row justify-content-center ssrp-absolute' style={{ marginTop: '4.5rem' }}>
         <h3 className='d-inline-block'></h3>
-      </div>
+      </div> */}
       <div className='mt-3'>
       <div className='row justify-content-sm-center justify-content-lg-end m-3'>
 
@@ -32,6 +32,7 @@ function Home() {
       {/* <div className='m-4'>
         <p className='Slogan'>Launching dreams into orbits</p>
       </div> */}
+    </div>
     </div>
   );
 }
