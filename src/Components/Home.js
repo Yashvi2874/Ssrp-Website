5aa9f808e5e07523e97aa57bca7f2bdd8c72b63e
Om Thanage <om.thanage@somaiya.edu>
2024-04-05 23:43:28 +0530

    Remove animation from home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Home.css';
-import TypeIt from 'typeit-react';
 import { Canvas } from '@react-three/fiber';
 import { Experience } from './Experience';
 
@@ -10,8 +9,8 @@ function Home() {
     <div className='Home' id='home'>
     <div className='text-center text-white m-4'>
       <div className='row justify-content-center ssrp-absolute'>
-      <TypeIt className='typeit-text' options={{ strings: "SSRP", speed: 50, waitUntilVisible: true, cursor: false }} />
-<TypeIt className='typeit-text' id='small-type' options={{ strings: "Somaiya Space Research Project", speed: 30, waitUntilVisible: true, cursor: false }} />
+      <div className='typeit-text'>SSRP</div>
+      <div className='typeit-text' id='small-type'>Somaiya Space Research Project</div>
       <p className='Slogan'>Launching dreams into orbits</p>
       </div>
       {/* <div className='row justify-content-center ssrp-absolute' style={{ marginTop: '4.5rem' }}>
