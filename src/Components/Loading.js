5dd1ac220e3ef65dfadfcc5f4f1943c9f13db181
Om Thanage <om.thanage@somaiya.edu>
2024-04-07 21:54:02 +0530

    Fix size of loading

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -5,8 +5,8 @@ function Loading() {
   return (
     <div className='Loading'>
       <div className="container1">
-        <div className="text-container left">RP</div>
-        <div className="text-container right">SS</div>
+        <div className="text-container left">CE</div>
+        <div className="text-container right">SP</div>
         <div className="ball-big">
           <div className="plane"></div>
           <div className="plane" ></div>
