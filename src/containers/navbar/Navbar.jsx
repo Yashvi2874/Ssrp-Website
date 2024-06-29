2b953e281d314f7af5aa15a58d0a9ca0d07cbdf5
Om Thanage <om.thanage@somaiya.edu>
2024-06-29 15:51:45 +0530

    Fix navbar and home page styles

--- src/containers/navbar/Navbar.jsx
+++ src/containers/navbar/Navbar.jsx
@@ -1,3 +1,132 @@
+// import React, { useEffect, useState } from "react";
+// import "./NavBar.css";
+// import { useCursorContext } from "../../components/cursor/CursorContext";
+
+// const NavBar = () => {
+//   const [sticky, setSticky] = useState(false);
+//   const { textEnter, textLeave } = useCursorContext();
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggle = () => {
+//     setIsOpen(!isOpen);
+//     console.log("Ok")
+//   };
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       if (window.scrollY > 20) {
+//         setSticky(true);
+//       } else {
+//         setSticky(false);
+//       }
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+
+//   return (
+    
+//     <nav className={sticky ? "sticky" : ""}>
+//       <div className="nav-content">
+//         <div className="logo">
+//         <img className='navbar-logo'src='/assets/images/about_images/logo.png' alt='Ssrp-logo'></img>
+//         <img className='navbar-logo'src='/assets/images/Trust_logo/somaiya white.png' alt='Somaiya_Vidyavihar'></img>
+//         <img className='navbar-logo'src='/assets/images/Trust_logo/somaiya trust.png' alt='Somaiya_Vidyavihar_University'></img>
+//         </div>
+//         <div className="toggle-button" onClick={toggle}>≡</div>
+//         {
+//           isOpen && (
+//             <div className="nav-links">
+//               <ul>
+//                 <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+//                   <div className="navigation">
+//                     <a className="nav-link active" aria-current="page" href="/">
+//                       Home
+//                     </a>
+//                   </div>
+//                 </li>
+//                 <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+//                   <div className="navigation">
+//                     <a className="nav-link active" aria-current="page" href="/project">
+//                       Projects
+//                     </a>
+//                   </div>
+//                 </li>
+//                 <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+//                   <div className="navigation">
+//                     <a className="nav-link active" aria-current="page" href="/community">
+//                       Community
+//                     </a>
+//                   </div>
+//                 </li>
+//                 <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+//                   <div className="navigation">
+//                     <a className="nav-link active" aria-current="page" href="/events">
+//                       Events
+//                     </a>
+//                   </div>
+//                 </li>
+//                 <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+//                   <div className="navigation">
+//                     <a className="nav-link active" aria-current="page" href="/crowdfunding">
+//                       Crowd-Funding
+//                     </a>
+//                   </div>
+//                 </li>
+//                 <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+//                   <div className="navigation">
+//                     <a className="nav-link active" aria-current="page" href="/contact">
+//                       Contact US
+//                     </a>
+//                   </div>
+//                 </li>
+//               </ul>
+//             </div>
+//           )
+//         }
+//         <ul className="nav-links">
+//         <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+//                    <div className="navigation">
+//                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+//                    </div>
+//                  </li>
+//                  <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+//                  <div className="navigation">
+//                    <a className="nav-link active" aria-current="page" href="/project">Projects</a>
+//                  </div>
+//                  </li>
+//                  <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+//                  <div className="navigation">
+//                    <a className="nav-link active" aria-current="page" href="/community">Community</a>
+//                  </div>
+//                  </li>
+//                  <li className="nav-item target"onMouseEnter={textEnter} onMouseLeave={textLeave}>
+//                  <div className="navigation">
+//                    <a className="nav-link active" aria-current="page" href="/events">Events</a>
+//                  </div>
+//                  </li>
+//                  <li className="nav-item target"onMouseEnter={textEnter} onMouseLeave={textLeave}>
+//                  <div className="navigation">
+//                    <a className="nav-link active" aria-current="page" href="/crowdfunding">Crowd-Funding</a>
+//                  </div>
+//                  </li>
+//                  <li className="nav-item target"onMouseEnter={textEnter} onMouseLeave={textLeave}>
+//                  <div className="navigation">
+//                    <a className="nav-link active" aria-current="page" href="/contact">Contact US</a>
+//                  </div>
+//                  </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default NavBar;
+
 import React, { useEffect, useState } from "react";
 import "./NavBar.css";
 import { useCursorContext } from "../../components/cursor/CursorContext";
@@ -5,6 +134,12 @@ import { useCursorContext } from "../../components/cursor/CursorContext";
 const NavBar = () => {
   const [sticky, setSticky] = useState(false);
   const { textEnter, textLeave } = useCursorContext();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+    console.log("Ok")
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -23,45 +158,95 @@ const NavBar = () => {
   }, []);
 
   return (
-    
     <nav className={sticky ? "sticky" : ""}>
       <div className="nav-content">
         <div className="logo">
-        <img className='navbar-logo'src='/assets/images/about_images/logo.png' alt='Ssrp-logo'></img>
-        <img className='navbar-logo'src='/assets/images/Trust_logo/somaiya white.png' alt='Somaiya_Vidyavihar'></img>
-        <img className='navbar-logo'src='/assets/images/Trust_logo/somaiya trust.png' alt='Somaiya_Vidyavihar_University'></img>
+          <img className='navbar-logo' src='/assets/images/about_images/logo.png' alt='Ssrp-logo'></img>
+          <img className='navbar-logo' src='/assets/images/Trust_logo/somaiya white.png' alt='Somaiya_Vidyavihar'></img>
+          <img className='navbar-logo' src='/assets/images/Trust_logo/somaiya trust.png' alt='Somaiya_Vidyavihar_University'></img>
         </div>
-        <ul className="nav-links">
-        <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
-                   <div className="navigation">
-                   <a className="nav-link active" aria-current="page" href="/">Home</a>
-                   </div>
-                 </li>
-                 <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
-                 <div className="navigation">
-                   <a className="nav-link active" aria-current="page" href="/project">Projects</a>
-                 </div>
-                 </li>
-                 <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
-                 <div className="navigation">
-                   <a className="nav-link active" aria-current="page" href="/community">Community</a>
-                 </div>
-                 </li>
-                 <li className="nav-item target"onMouseEnter={textEnter} onMouseLeave={textLeave}>
-                 <div className="navigation">
-                   <a className="nav-link active" aria-current="page" href="/events">Events</a>
-                 </div>
-                 </li>
-                 <li className="nav-item target"onMouseEnter={textEnter} onMouseLeave={textLeave}>
-                 <div className="navigation">
-                   <a className="nav-link active" aria-current="page" href="/crowdfunding">Crowd-Funding</a>
-                 </div>
-                 </li>
-                 <li className="nav-item target"onMouseEnter={textEnter} onMouseLeave={textLeave}>
-                 <div className="navigation">
-                   <a className="nav-link active" aria-current="page" href="/contact">Contact US</a>
-                 </div>
-                 </li>
+        <div className="toggle-button" onClick={toggle}>≡</div>
+        {
+          isOpen && (
+            <div className="nav-links mobile">
+              <ul>
+                <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+                  <div className="navigation">
+                    <a className="nav-link active" aria-current="page" href="/">
+                      Home
+                    </a>
+                  </div>
+                </li>
+                <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+                  <div className="navigation">
+                    <a className="nav-link active" aria-current="page" href="/project">
+                      Projects
+                    </a>
+                  </div>
+                </li>
+                <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+                  <div className="navigation">
+                    <a className="nav-link active" aria-current="page" href="/community">
+                      Community
+                    </a>
+                  </div>
+                </li>
+                <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+                  <div className="navigation">
+                    <a className="nav-link active" aria-current="page" href="/events">
+                      Events
+                    </a>
+                  </div>
+                </li>
+                <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+                  <div className="navigation">
+                    <a className="nav-link active" aria-current="page" href="/crowdfunding">
+                      Crowd-Funding
+                    </a>
+                  </div>
+                </li>
+                <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+                  <div className="navigation">
+                    <a className="nav-link active" aria-current="page" href="/contact">
+                      Contact US
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          )
+        }
+        <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+          <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+            <div className="navigation">
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
+            </div>
+          </li>
+          <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+            <div className="navigation">
+              <a className="nav-link active" aria-current="page" href="/project">Projects</a>
+            </div>
+          </li>
+          <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+            <div className="navigation">
+              <a className="nav-link active" aria-current="page" href="/community">Community</a>
+            </div>
+          </li>
+          <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+            <div className="navigation">
+              <a className="nav-link active" aria-current="page" href="/events">Events</a>
+            </div>
+          </li>
+          <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+            <div className="navigation">
+              <a className="nav-link active" aria-current="page" href="/crowdfunding">Crowd-Funding</a>
+            </div>
+          </li>
+          <li className="nav-item target" onMouseEnter={textEnter} onMouseLeave={textLeave}>
+            <div className="navigation">
+              <a className="nav-link active" aria-current="page" href="/contact">Contact US</a>
+            </div>
+          </li>
         </ul>
       </div>
     </nav>
