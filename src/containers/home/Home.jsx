e2f6ac451ed7c858d260762340e2990ff71a5d4a
SwarniChouhan-20 <swarni.c@somaiya.edu>
2024-07-04 15:38:09 +0530

    created cards for about us and added google form.

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -65,43 +65,31 @@ function Home() {
 
       <div className='About'>
         <div className='container-about'>
-          <div className="text1" data-aos="fade-right">
-            <p className='item' onMouseEnter={textEnter} onMouseLeave={textLeave}>Our Cosmic Identity</p>
+          <div className="text1" data-aos="flip-down">
+            <p className='item1' onMouseEnter={textEnter} onMouseLeave={textLeave}>Our Cosmic Identity</p>
+            <img className='about-img' src='/assets/images/about_images/logo.png' alt='ssrp logo'/>
             <p className='about-text' onMouseEnter={textEnter} onMouseLeave={textLeave}>The Somaiya Space Research Project (SSRP), a new student-led initiative launched on January 16, 2024, at KJ Somaiya College of Engineering, focuses on developing and launching the KJSCE StudentSAT and advancing space exploration. Our initiatives combine research with participation in competitions like Spaceport America, the International Rover Challenge, and CANSAT, aiming to make a significant impact in space research.</p>
           </div>
 
-          <div className='text2' data-aos="fade-left">
-            <div className='item flex' onMouseEnter={textEnter} onMouseLeave={textLeave}  >Galactic Vision</div>
+          <div className='text2' data-aos="flip-down">
+            <div className='item' onMouseEnter={textEnter} onMouseLeave={textLeave}  >Galactic Vision</div>
+            <img className='about-img' src='/assets/images/about_images/futuregoalsnew.png' alt='ssrp future goals images'/>
             <p className='about-text' onMouseEnter={textEnter} onMouseLeave={textLeave}>We combine research with renowned competitions like IRC, CANSAT, and IROC-U to expand knowledge and validate student contributions. Competitions enhance problem-solving skills, while in-depth research propels space exploration and drives cutting-edge technologies. By tackling present challenges, we lay the groundwork for future innovations with lasting impact.</p>
           </div>
 
-          <div className="text3" data-aos="fade-right">
+          <div className="text3" data-aos="flip-down">
             <p className='item' onMouseEnter={textEnter} onMouseLeave={textLeave}>Interstellar Allies</p>
+            <div className='Carousel' data-aos="fade-left"><ControlledCarousel /></div>
+            <p className='collab' >KJSIT: K.J. Somaiya Institute of Technology</p>
             <p className='about-text' onMouseEnter={textEnter} onMouseLeave={textLeave}>Our partnership with KJSIT for the HAMSAT project is now solidified, marking a significant step forward in our collaboration. Together, we're set to innovate in satellite technology, pooling our expertise and resources for groundbreaking solutions. This alliance reflects our shared commitment to pushing technological boundaries.</p>
           </div>
 
-          <div className="text4">
-            <p className='item' data-aos="fade-down-right" onMouseEnter={textEnter} onMouseLeave={textLeave}>Power Our Journey</p>
-            <p className='about-text' onMouseEnter={textEnter} onMouseLeave={textLeave} data-aos="fade-down-left">The Somaiya Space Research Project, established this year, pioneers advancements in space exploration through innovative research, focusing on developing the StudentSAT. Our passionate team aims to make significant contributions to the global space community. We invite sponsors to partner with us, supporting our mission to push the boundaries of space exploration. Sponsors will contribute to cutting-edge innovation and gain international recognition. Together, we embark on a remarkable journey to shape the future of space exploration.</p>
+          <div className="text4" data-aos="flip-down">
+            <p className='item' onMouseEnter={textEnter} onMouseLeave={textLeave}>Power Our Journey</p>
+            <p className='about-text' onMouseEnter={textEnter} onMouseLeave={textLeave}>The Somaiya Space Research Project, established this year, pioneers advancements in space exploration through innovative research, focusing on developing the StudentSAT. Our passionate team aims to make significant contributions to the global space community. We invite sponsors to partner with us, supporting our mission to push the boundaries of space exploration. Sponsors will contribute to cutting-edge innovation and gain international recognition. Together, we embark on a remarkable journey to shape the future of space exploration.</p>
           </div>
 
-          <div className="text5">
-            <p className='item'>
-              <img className='about-img' src='/assets/images/about_images/logo.png' alt='ssrp logo' data-aos="fade-left" />
-            </p>
-          </div>
-
-          <div className="text6">
-            <p className='item'>
-              <img className='about-img' src='/assets/images/about_images/futuregoalsnew.png' alt='ssrp future goals images' data-aos="fade-right" />
-            </p>
-          </div>
-
-          <div className="text7">
-            <div className='Carousel' data-aos="fade-left"><ControlledCarousel /></div>
-            <p className='collab' data-aos="fade-left">KJSIT: K.J. Somaiya Institute of Technology</p>
-          </div>
-          <div className='text8' data-aos="fade-left">
+          {/* <div className='text8' data-aos="fade-left">
             <p className='item' onMouseEnter={textEnter} onMouseLeave={textLeave}>Galactic Vision</p>
           </div>
           <div className='text9' data-aos="fade-right">
@@ -110,7 +98,7 @@ function Home() {
 
           <div className='text10'>
             <p className='item' onMouseEnter={textEnter} onMouseLeave={textLeave}>Our Cosmic Identity</p>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
