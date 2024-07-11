6967f91339cdc3025050852ecd84f8a97c7ac370
Om Thanage <om.thanage@somaiya.edu>
2024-07-12 00:26:16 +0530

    Remove unused footer from EventPage

--- src/pages/events/EventPage.jsx
+++ src/pages/events/EventPage.jsx
@@ -1,6 +1,5 @@
 import  { useState, useEffect} from 'react'
 import Navbar from '../../containers/navbar/Navbar';
-import Footer from '../../containers/footer/Footer';
 import Loading from '../../components/loading/Loading';
 import BoxContainer from '../../containers/events/BoxContainer';
 
