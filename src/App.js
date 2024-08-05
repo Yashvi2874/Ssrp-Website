ecf68498263d61e8fef765782a1e29b8f09be477
Om Thanage <om.thanage@somaiya.edu>
2024-08-06 01:19:29 +0530

    Remove unnecessary files and update EventDetails and EventDescriptionPage Use mapping method to display event details

--- src/App.js
+++ src/App.js
@@ -1,171 +1,141 @@
-//import { SpeedInsights } from "@vercel/speed-insights/react";
 import './App.css';
-import  { useEffect} from 'react';
-import ProjectPage from './pages/project/ProjectPage'
-import StarBackground from './components/background/Background';
-import NotFound from './components/Not found page/NotFound';
+import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AOS from 'aos';
-import ContactPage from './pages/contact/ContactPage';
+import { CursorProvider } from './components/cursor/CursorContext';
+import StarBackground from './components/background/Background';
+import NotFound from './components/Not found page/NotFound';
 import HomePage from './pages/home/HomePage';
 import TeamPage from './pages/team/TeamPage';
 import EventPage from './pages/events/EventPage';
+import ProjectPage from './pages/project/ProjectPage';
 import ProjectDescriptionPage from './pages/project/ProjectDescriptionPage';
-import { CursorProvider } from './components/cursor/CursorContext';
+import ContactPage from './pages/contact/ContactPage';
 import CrowdFundingPage from './pages/crowdfunding/CrowdFundingPage';
-import EventDescriptionPage from './pages/events/EventDescriptionPage';
-
+import EventDescriptionPage from './pages/events/EventDescriptionPage'; // Import your EventDescriptionPage
 
 function App() {
-    
-    useEffect(()=>{
-      AOS.init({duration:"2000"});
-    },[]);
+  useEffect(() => {
+    AOS.init({ duration: "2000" });
+  }, []);
 
-
-  
   return (
-    <CursorProvider >
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={
-          <div className="App">
-            <div className="stars">
-              <StarBackground />
-            </div>
-            <div className="absolute">
-            
-              <div className="relative" style={{ marginTop: "-15vh"}}>
-                <HomePage />
-              </div>
-            </div>
-          </div>
-        } />
-      
-        
-      <Route path="/community" element={
-        
-        <div className="App">
-            <div className="stars">
-              <StarBackground />
-            </div>
-            <div className="absolute">
-              <div className="relative">
-                <TeamPage />
-              </div>
-            </div>
-          </div>} />
-
-          <Route path="/events" element={
-        
-        <div className="App">
-            <div className="stars">
-              <StarBackground />
-            </div>
-            <div className="absolute">
-            <div className="relative" style={{
-                marginTop: "-15vh"
-              }}>
-                <EventPage />
-                
-            </div>
-            </div>
-          </div>} />
-
-
-          
-            <Route path="/event/:eventId" element={
+    <CursorProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
             <div className="App">
-            <div className="stars">
-              <StarBackground />
-            </div>
-      
-            <div className="absolute">
-              <div className="relative">
-                <EventDescriptionPage/>
+              <div className="stars">
+                <StarBackground />
+              </div>
+              <div className="absolute">
+                <div className="relative" style={{ marginTop: "-15vh" }}>
+                  <HomePage />
+                </div>
               </div>
             </div>
-          </div>
           } />
-
+          <Route path="/community" element={
+            <div className="App">
+              <div className="stars">
+                <StarBackground />
+              </div>
+              <div className="absolute">
+                <div className="relative">
+                  <TeamPage />
+                </div>
+              </div>
+            </div>
+          } />
+          <Route path="/events" element={
+            <div className="App">
+              <div className="stars">
+                <StarBackground />
+              </div>
+              <div className="absolute">
+                <div className="relative" style={{ marginTop: "-15vh" }}>
+                  <EventPage />
+                </div>
+              </div>
+            </div>
+          } />
           <Route path="/project" element={
-        
-        <div className="App">
-            <div className="stars">
-              <StarBackground />
-            </div>
-            <div className="absolute">
-              <div className="relative">
-                <ProjectPage />
+            <div className="App">
+              <div className="stars">
+                <StarBackground />
+              </div>
+              <div className="absolute">
+                <div className="relative">
+                  <ProjectPage />
+                </div>
               </div>
             </div>
-          </div>} />
-
+          } />
           <Route path="/projects" element={
-
-        
-        <div className="App">
-            <div className="stars">
-              <StarBackground />
-            </div>
-            <div className="absolute">
-              <div className="relative">
-                <ProjectPage />
+            <div className="App">
+              <div className="stars">
+                <StarBackground />
+              </div>
+              <div className="absolute">
+                <div className="relative">
+                  <ProjectPage />
+                </div>
               </div>
             </div>
-          </div>} />
-
+          } />
           <Route path="/project/:projectId" element={
             <div className="App">
-            <div className="stars">
-              <StarBackground />
-            </div>
-            <div className="absolute">
-              <div className="relative">
-                <ProjectDescriptionPage />
+              <div className="stars">
+                <StarBackground />
+              </div>
+              <div className="absolute">
+                <div className="relative">
+                  <ProjectDescriptionPage />
+                </div>
               </div>
             </div>
-          </div>
           } />
-
-        <Route path="/contact" element={
-          <div className="App">
-            <div className="stars">
-              <StarBackground />
-            </div>
-            <div className="absolute">
-              <div className="relative">
-                <ContactPage />
+          <Route path="/contact" element={
+            <div className="App">
+              <div className="stars">
+                <StarBackground />
+              </div>
+              <div className="absolute">
+                <div className="relative">
+                  <ContactPage />
+                </div>
               </div>
             </div>
-          </div>
-        } />
-
-        <Route path="/crowdfunding" element={
-          <div className="App">
-            <div className="stars">
-              <StarBackground />
-            </div>
-            <div className="absolute">
-              <div className="relative">
-                <CrowdFundingPage />
+          } />
+          <Route path="/crowdfunding" element={
+            <div className="App">
+              <div className="stars">
+                <StarBackground />
+              </div>
+              <div className="absolute">
+                <div className="relative">
+                  <CrowdFundingPage />
+                </div>
               </div>
             </div>
-          </div>
-        } />
-
-
-        <Route path="*" element={<NotFound />} />
-
-      </Routes>
-    </BrowserRouter>
+          } />
+          <Route path="/event/:eventId" element={ // Add this route
+            <div className="App">
+              <div className="stars">
+                <StarBackground />
+              </div>
+              <div className="absolute">
+                <div className="relative">
+                  <EventDescriptionPage />
+                </div>
+              </div>
+            </div>
+          } />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </CursorProvider>
-          );
+  );
 }
 
 export default App;
-
-
-
-
-
--- src/pages/events/EventDescriptionPage.jsx
+++ src/pages/events/EventDescriptionPage.jsx
@@ -1,7 +1,5 @@
 import Navbar from '../../containers/navbar/Navbar';
 import Footer from '../../containers/footer/Footer';
-import EventDescription from '../../containers/events/EventDescription';
-import DetailsPage from '../../containers/events/DetailsPage';
 
 
 
@@ -9,7 +7,6 @@ function EventDescriptionPage() {
   return (
     <>
         <Navbar />
-        <DetailsPage />
         <Footer />
 
         
