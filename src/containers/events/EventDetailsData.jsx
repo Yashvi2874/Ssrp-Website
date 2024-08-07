b528c5545aecf69ffe31158e1341befb0c25e689
Siyanair22 <siya.nair2209@gmail.com>
2024-08-07 13:01:25 +0530

    Added the event information.

--- src/containers/events/EventDetailsData.jsx
+++ src/containers/events/EventDetailsData.jsx
@@ -2,12 +2,14 @@ const Events = [
     {
         id: 1,
         title: 'TRIO-CONCLAVE',
+        description:"The trio conclave was organized in KJSCE, with the main objective of it being a brainstorming get-together, intended to foster a more vibrant and result-oriented collaboration between the Academia, and R&D Institutions. Collaboration among these three entities is vital and a game changer for the development of India, in line with Vikasit Bharat@2047. In addition to conferences, panel discussions, and presentations, there were stalls set up on the SVU Campus, in the Gargi Plaza highlighting various ventures and projects supporting the objectives of the conclave. One of these was that of or mega-project, Somaiya Space Research Project (SSRP) of KJSCE. We displayed our progress in this stall, where we also received guidance from the Director of Satish Dhawan Space Centre, Shri. Arumugam Rajarajan. The exposure and interactions, the team SSRP got through were invaluable. The event was concluded on 3 April 2024.",
         src: 'moon.jpg',
         color: "orange",
     },
     {
         id: 2,
         title: 'ASTROGAZING',
+        description: "Overnight Astro Gazing Event by SSRP\nThe Astronomical Society of Somaiya Space Research Project (SSRP) under KJSCE hosted a magical Overnight Astro Gazing event that brought the wonders of the cosmos closer to the Somaiya community. On April 19th, 2024, over 65 students and faculty gathered for an astronomical adventure, kicking off with an exciting scavenger hunt that sparked curiosity and camaraderie. Here is a glimpse of our stellar night:\nEvent Highlights:\nCreative Cosmos:\nParticipants expressed their creativity by designing cosmic-themed t-shirts, creating vibrant mementos of the event.\nStargazing Extravaganza:\nUsing a 12-inch Explore Scientific Newtonian Truss Tube Telescope and two 5/6 inch Newtonian Telescopes, participants marveled at lunar craters. Guided by certified astronomers, they enjoyed awe-inspiring views of the moon's surface, followed by a delightful buffet dinner.\nPlanetary Exploration and Deep Space Wonders:\nThe second star gazing session offered extraordinary views of planets and deep space objects, leaving everyone in awe of the vastness and beauty of space.\nAstrophotography and Career Guidance:\nAspiring astrophotographers captured the night sky with guidance from experts, and experienced astronomers provided career advice.\nSunrise and Solar Observations:\nAs dawn approached, participants observed sunspots through solar-filtered telescopes, concluding the event with a fitting finale.\nWe look forward to many more nights of stargazing and exploration within our community!",
         src: 'moon.jpg',
         color: "purple",
     }
